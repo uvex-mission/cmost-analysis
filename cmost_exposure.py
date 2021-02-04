f60bab24405dce5fad746f9c7d3fdd14a4ace99a
Hannah <hannah.earnshaw@googlemail.com>
2021-02-03 19:26:53 -0800

    Subframe mean/median/variance
    Implemented functions to calculate statistical properties of given subframe. Created utility functions for loading multiple frames. Replicated linearity and PTC plots in an example notebook.

--- cmost_exposure.py
+++ cmost_exposure.py
@@ -1,9 +1,8 @@
 ''' Functions relating to opening and manipulating CMOST images'''
-from __future__ import division, print_function
 import os
-import re
 import numpy as np
 from astropy.io import fits
+from astropy.table import Table
 from datetime import datetime
 
 class Exposure():
@@ -141,6 +140,11 @@ class Exposure():
 	def get_info(self):
 		'''
 		Output useful image info in a nice readable format
+		
+		Returns
+		-------
+		info_string : string
+			String containing properties from FITS header and number of frames
 		'''
 		info_string = """ Properties: 
 		Readout mode: {} 
@@ -157,25 +161,132 @@ class Exposure():
 					self.gain, len(self.raw_frames))
 		return info_string
 
-	def get_mean(self):
-		x = 'To-do' 
+	def get_mean(self, subframe):
+		'''
+		Calculate mean of subframe across all useable frames
+		
+		Parameters
+		----------
+		subframe : array, list or tuple
+			Indices of the subframe in format (x1, x2, y1, y2)
+			
+		Returns
+		-------
+		Mean of subframe across all useable frames
+		'''
+		x1, x2, y1, y2 = subframe
+		m = np.mean(self.cds_frames[:,y1:y2,x1:x2])
+		return m
 	
-	def get_median(self):
-		x = 'To-do' 
+	def get_median(self, subframe):
+		'''
+		Calculate median of subframe across all useable frames
+		
+		Parameters
+		----------
+		subframe : array, list or tuple
+			Indices of the subframe in format (x1, x2, y1, y2)
+			
+		Returns
+		-------
+		Median of subframe across all useable frames
+		'''
+		x1, x2, y1, y2 = subframe
+		m = np.median(self.cds_frames[:,y1:y2,x1:x2])
+		return m
 	
-	def get_variance(self):
-		x = 'To-do' 
+	def get_variance(self, subframe):
+		'''
+		Calculate variance of subframe of difference between first two useable frames
+		
+		Parameters
+		----------
+		subframe : array, list or tuple
+			Indices of the subframe in format (x1, x2, y1, y2)
+			
+		Returns
+		-------
+		Variance of the subframe if more than one useable frame, otherwise 0
+		'''
+		x1, x2, y1, y2 = subframe
+		if len(self.cds_frames) > 1:
+			diff = self.cds_frames[1,y1:y2,x1:x2] - self.cds_frames[0,y1:y2,x1:x2]
+			var = np.var(diff) / 2
+			return var
+		else:
+			print('WARNING: Not enough frames to calculate variance')
+			return 0
 
+'''
+	Utility functions for loading Exposure objects
+'''
 
-# To-do: 
+def load_by_file_prefix(file_prefix):
+	'''
+	Load all FITS files that begin with the filepath string provided
+	
+	Parameters
+    ----------
+    file_prefix : string
+    	Common path to desired FITS files, including filename prefix
+    	
+    Returns
+    -------
+    List of Exposure objects
+	'''
+	directory = os.path.dirname(file_prefix)
+	filepaths = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+	
+	return load_by_filepath(filepaths)
+	
+def load_by_filepath(filepaths):
+	'''
+	Load FITS files from filepaths provided
+	
+	Parameters
+    ----------
+    filepaths : str array
+    	Array of filepaths to desired FITS files
+    	
+    Returns
+    -------
+    List of Exposure objects
+	'''
+	exposures = [Exposure(f) for f in filepaths[:-1]]
+	return exposures
 
-#def get_frames_by_fileprefix(fileprefix):
-# Behaviour as in Hannah's code
 	
-#def get_frames_by_filenames(filenames):
-# Pass an array of filenames, return list of objects
+def scan_headers(directory):
+	'''
+	Scan the FITS headers in given directory and return file details
 	
-#def scan_headers():
-# Scan headers of FITS files in the directory you specify
-# Return a an astropy Table of header contents 
+	Parameters
+    ----------
+    directory : string
+    	Path to the directory to scan
+    	
+    Returns
+    -------
+    Astropy Table of FITS header contents of files in given directory 
+    '''	
+	table_rows = []
+	for f in os.listdir(directory):
+		filepath = os.path.join(directory, f)
+		if os.path.isfile(filepath):
+    		# Open fits file to view header
+			cmost_file = fits.open(filepath)
+			hdr = cmost_file[0].header
+			cmost_file.close()
+    		
+			# Create table row
+			table_rows.append((filepath, hdr['READOUTM'], datetime.fromisoformat(hdr['DATE']),
+							float(hdr.get('EXPTIME',-1)), float(hdr.get('LED',-1)), 
+							float(hdr.get('TEMP',-1)), hdr.get('CAMERAID',''), 
+							hdr.get('DETID',''), hdr.get('GAIN','')))
+	
+	# Construct astropy table
+	t = Table(rows=table_rows, 
+				names=('FILEPATH', 'READOUTM', 'DATE', 'EXPTIME', 'LED', 'TEMP', 'CAMERAID', 'DETID', 'GAIN'))
+	
+	return t
 
