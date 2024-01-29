223274f2a1860d7bac012d095ccd16d813b5d79d
Hannah <hannah.earnshaw@googlemail.com>
2024-01-29 08:42:19 -0800

    Manifest file creation
    Code to create a manifest file of a given top-level data directory by scanning the headers of each subfolder

---
+++ cmost_manifest.py
@@ -0,0 +1,47 @@
+'''
+    Script to create a manifest of available CMOST data
+    
+    Usage: cmost_manifest.py [DATA_DIR] [FILENAME]
+'''
+import os, sys
+sys.path.append('..')
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table, vstack
+from cmost_exposure import scan_headers
+
+def create_manifest(data_dir, filename):
+    # Load manifest file
+    m = fits.open(filename)
+    manifest = Table(m[1].data)
+    directories = manifest['DIRECTORY']
+    m.close()
+
+    # Loop through all available directories
+    for dir in os.scandir(data_dir):
+        if dir.is_dir():
+            # Check if this directory is registered in the manifest
+            # If so, skip
+            if dir.name in directories:
+                continue
+            
+            # Scan FITS file headers
+            header_table = scan_headers(data_dir+'/'+dir.name)
+            
+            # scan_headers() returns False if directory contains no files
+            if header_table:
+                header_table.add_column([dir.name]*len(header_table),name='DIRECTORY')
+                manifest = vstack([manifest,header_table])
+            
+    manifest.write(filename)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        data_dir = input('Data directory: ')
+        filename = input('Manifest filename: ')
+    else:
+        data_dir = sys.argv[1]
+        filename = sys.argv[2]
+    
+    create_manifest(data_dir, filename)
