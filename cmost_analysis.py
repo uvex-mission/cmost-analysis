d86b6ff31ca82b04e3013659d02696cfccfbd0fd
Hannah <hannah.earnshaw@googlemail.com>
2024-04-25 12:40:03 -0700

    Improvements to histogram pages; added well-depth and linearity report

--- cmost_analysis.py
+++ cmost_analysis.py
@@ -85,50 +85,22 @@ def standard_analysis_products(dirname):
                 n_frame = len(bifr.cds_frames)
                 
                 for i, med_bias in enumerate(med_frames):
+                    
                     mmin, mmax = min(med_bias.flatten()), max(med_bias.flatten())
-                    med_range = int(np.ceil(mmax) - np.floor(mmin))
-
-                    fig = plt.figure(figsize=[8.5,11],dpi=300)
-                    plt.suptitle(f'Bias frames - gain: {gain[i]}')
-                
-                    bias_hist, bin_edges = np.histogram(med_bias,bins=100)
-                    bias_hist2, bin_edges2 = np.histogram(med_bias,bins=med_range) # i.e. binsize=1
-
-                    # Output a median frame image and histogram
-                    ax1 = plt.subplot2grid((5,2), (0,0), rowspan=2, colspan=2)
-                    ax2 = plt.subplot2grid((5,2), (2,0), rowspan=2)
-                    ax3 = plt.subplot2grid((5,2), (2,1), rowspan=2)
-                    
-                    plt.sca(ax1)
-                    plt.imshow(med_bias, vmin=0, vmax=50)
-                    plt.colorbar(label='ADU', shrink=0.9)
-                    
-                    plt.sca(ax2)
-                    plt.stairs(bias_hist,edges=bin_edges)
-                    plt.xlabel('ADU')
-                    plt.ylabel('Pixel Count')
-                    plt.text(0.5,0.9,'Whole range',transform=ax2.transAxes)
-                    plt.semilogy()
-                    
-                    plt.sca(ax3)
-                    plt.stairs(bias_hist2,edges=bin_edges2)
-                    plt.xlabel('ADU')
-                    plt.xlim(np.percentile(med_bias,[1.e-2,100-1.e-2]))
-                    plt.text(0.3,0.9,'0.01 - 99.99 percentile',transform=ax3.transAxes)
-                    plt.semilogy()
+                    n_bad = np.sum(med_bias > 50)
+                    bad_percent = n_bad / med_bias.size * 100.
                     
                     summary_text = f'Gain mode: {gain[i]}\n'
                     summary_text += f'Median of {n_frame} minimum-length exposures\n'
-                    summary_text += f'Min median pixel value: {mmin} ADU; max median pixel value: {mmax} ADU'
+                    summary_text += f'Min median pixel value: {mmin} ADU; max median pixel value: {mmax} ADU\n'
+                    summary_text += f'Pixels > 50 ADU: {n_bad}, {bad_percent:.3f}%\n'
                     
-                    plt.text(0.0, -0.3, summary_text, transform=ax2.transAxes, verticalalignment='top')
-                
-                    plt.subplots_adjust(hspace=0.25)
-                    pdf.savefig()
-                    plt.close()
+                    # Create a standard histogram page and save it to the pdf
+                    hist_page(pdf, med_bias, f'Bias frames - gain: {gain[i]}', summary_text)
             
         # Standard operating darks
         # Get darks
+        #dark_present = 0
         if dark_present:
             for mode in ['FUVdark','NUVdark','NUVdarkguiding']:
                 if mode == 'FUVdark':
@@ -179,47 +151,13 @@ def standard_analysis_products(dirname):
                 
                 for j, med_dark in enumerate(frames):
                     mmin, mmax = min(med_dark.flatten()), max(med_dark.flatten())
-                    med_range = int(np.ceil(mmax) - np.floor(mmin))
-
-                    fig = plt.figure(figsize=[8.5,11],dpi=300)
-                    plt.suptitle(f'Dark frames - {mode} mode - {names[j]} frame')
-                
-                    dark_hist, bin_edges = np.histogram(med_dark,bins=100)
-                    dark_hist2, bin_edges2 = np.histogram(med_dark,bins=med_range) # i.e. binsize=1
-
-                    # Output a median frame image and histogram
-                    ax1 = plt.subplot2grid((5,2), (0,0), rowspan=2, colspan=2)
-                    ax2 = plt.subplot2grid((5,2), (2,0), rowspan=2)
-                    ax3 = plt.subplot2grid((5,2), (2,1), rowspan=2)
-                    
-                    plt.sca(ax1)
-                    plt.imshow(med_dark, vmin=0, vmax=50)
-                    plt.colorbar(label='ADU', shrink=0.9)
-                    
-                    plt.sca(ax2)
-                    plt.stairs(dark_hist,edges=bin_edges)
-                    plt.xlabel('ADU')
-                    plt.ylabel('Pixel Count')
-                    plt.text(0.5,0.9,'Whole range',transform=ax2.transAxes)
-                    plt.semilogy()
-                    
-                    plt.sca(ax3)
-                    plt.stairs(dark_hist2,edges=bin_edges2)
-                    plt.xlabel('ADU')
-                    plt.xlim(np.percentile(med_dark,[1.e-2,100-1.e-2]))
-                    plt.text(0.3,0.9,'0.01 - 99.99 percentile',transform=ax3.transAxes)
-                    plt.semilogy()
                     
                     summary_text = f'Frame: {names[j]}\n'
                     summary_text += f'Median of {n_frame} x {times[j]}s exposures\n'
                     summary_text += f'Min median pixel value: {mmin} ADU; max median pixel value: {mmax} ADU'
                     
-                    plt.text(0.0, -0.3, summary_text, transform=ax2.transAxes, verticalalignment='top')
-                
-                    plt.subplots_adjust(hspace=0.25)
-                    pdf.savefig()
-                    plt.close()
-                
+                    # Create a standard histogram page and save it to the pdf
+                    hist_page(pdf, med_dark, f'Dark frames - {mode} mode - {names[j]} frame', summary_text)
                 
                 # Also make a page of mean values over time
                 fig = plt.figure(figsize=[8.5,11],dpi=300)
@@ -240,23 +178,24 @@ def standard_analysis_products(dirname):
                 plt.sca(ax1)
                 plt.title(f'{names[0]}')
                 #plt.errorbar(s_starts,np.mean(short_low,axis=(1,2)),np.std(short_low,axis=(1,2)))
-                plt.plot(s_starts,np.mean(short_low,axis=(1,2)),color='k')
-                for j in range(n_cols): plt.plot(s_starts,sl_col_mean[j],alpha=0.5)
+                plt.plot(s_starts,np.mean(short_low,axis=(1,2)),color='k',label='Whole chip')
+                for j in range(n_cols): plt.plot(s_starts,sl_col_mean[j],alpha=0.5,label=f'Col {j}')
                 plt.ylabel('ADU')
                 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+                plt.legend(fontsize=10,ncol=6,loc=8,bbox_to_anchor=(0.5, -0.25))
                 
                 # Mean long-frame dark value over time
                 plt.sca(ax2)
                 plt.title(f'{names[1]}')
-                plt.plot(l_starts,np.mean(long_low,axis=(1,2)),color='k')
-                for j in range(n_cols): plt.plot(l_starts,ll_col_mean[j],alpha=0.5)
+                plt.plot(l_starts,np.mean(long_low,axis=(1,2)),color='k',label='Whole chip')
+                for j in range(n_cols): plt.plot(l_starts,ll_col_mean[j],alpha=0.5,label=f'Col {j}')
                 plt.ylabel('ADU')
                 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
                 
                 plt.sca(ax3)
                 plt.title(f'{names[2]}')
-                plt.plot(l_starts,np.mean(long_high,axis=(1,2)),color='k')
-                for j in range(n_cols): plt.plot(l_starts,lh_col_mean[j],alpha=0.5)
+                plt.plot(l_starts,np.mean(long_high,axis=(1,2)),color='k',label='Whole chip')
+                for j in range(n_cols): plt.plot(l_starts,lh_col_mean[j],alpha=0.5,label=f'Col {j}')
                 plt.xlabel('Time')
                 plt.ylabel('ADU')
                 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
@@ -265,11 +204,120 @@ def standard_analysis_products(dirname):
                 pdf.savefig()
                 plt.close()
     
-    # TODO: something for guiding rows
-    # TODO: optimize color bars
-    # TODO: flat report pages
+        if flat_present:
+            gain_label, sat_flats = [], []
+            exp_times, med_sig, var = [], [], []
+            for gain in ['hdr','high','low']:
+                flat_frames = load_by_file_prefix(f'{dirname}/{camera}_{detid}_flat_{gain}')
+                exp = np.array([ff.exp_time for ff in flat_frames])
+                max_exp, max_i = max(exp), np.argmax(exp)
+                
+                if gain == 'hdr':
+                    gain_label.extend(['high (dual-gain)', 'low (dual-gain)'])
+                    sat_flats.extend((flat_frames[max_i].cds_frames[0,0], flat_frames[max_i].cds_frames[0,1]))
+                    medians = np.array([ff.get_median((0,ff.dev_size[0],0,ff.dev_size[1])) for ff in flat_frames])
+                    #variance = NEED TWO FRAMES FOR THIS!!
+                    
+                    exp_times.extend((exp,exp))
+                    med_sig.extend((medians[:,0],medians[:,1]))
+                else:
+                    gain_label.append(gain)
+                    sat_flats.append(flat_frames[max_i].cds_frames[0])
+                    medians = np.array([ff.get_median((0,ff.dev_size[0],0,ff.dev_size[1])) for ff in flat_frames])
+                    #variance = NEED TWO FRAMES FOR THIS!!
 
+                    exp_times.append(exp)
+                    med_sig.append(medians)
 
+            # Well depth report
+            for i, flat in enumerate(sat_flats):
+                mmin, mmax = min(flat.flatten()), max(flat.flatten())
+                n_bad = np.sum(flat < 5000)
+                bad_percent = n_bad / flat.size * 100.
+                
+                summary_text = f'Gain mode: {gain_label[i]}\n'
+                summary_text += f'Illuminated {max_exp}s exposure (saturated)\n'
+                summary_text += f'Min median pixel value: {mmin} ADU; max median pixel value: {mmax} ADU\n'
+                summary_text += f'Pixels < 5000 ADU: {n_bad}, {bad_percent:.3f}%\n'
+                
+                # Create a standard histogram page and save it to the pdf
+                hist_page(pdf, flat, f'Saturated flat frame - gain: {gain_label[i]}', summary_text)
+                    
+            # Linearity and PTC plots
+            fig = plt.figure(figsize=[8.5,11],dpi=300)
+            plt.suptitle(f'Linearity and photon transfer curves')
+            
+            ax1 = plt.subplot2grid((2,1), (0,0))
+            ax2 = plt.subplot2grid((2,1), (1,0))
+            
+            # Linearity curve
+            plt.sca(ax1)
+            plt.title(f'Linearity')
+            for j in range(4): plt.scatter(exp_times[j],med_sig[j],label=f'{gain_label[j]}')
+            plt.xlabel('Exposure Time (s)')
+            plt.ylabel('Median Signal (ADU)')
+            plt.legend(fontsize=10,ncol=4,loc=8,bbox_to_anchor=(0.5, -0.2))
+                
+            # PTC (TBA)
+            plt.sca(ax2)
+            plt.title(f'Photon Transfer Curve')
+            #for j in range(4): plt.scatter(med_sig[j],variance[j],label=f'{gain_label[j]}')
+            plt.xlabel('Median Signal (ADU)')
+            plt.ylabel('Variance (ADU^2)')
+            
+            plt.tight_layout()
+            pdf.savefig()
+            plt.close()
+            
+            # Lin is Exp time vs Med Signal
+            # PTC is Med Signal vs Variance
+            
+        
+        # TODO: something for guiding rows
+        # TODO: flat report pages
+        # TODO: labels for mean dark plots
+
+def hist_page(pdf, data, title, summary_text):
+    '''
+    Default style of page showing a figure of the image, histograms, and summary text
+    '''
+    dmin, dmax = min(data.flatten()), max(data.flatten())
+    data_range = int(np.ceil(dmax) - np.floor(dmin))
+
+    fig = plt.figure(figsize=[8.5,11],dpi=300)
+    plt.suptitle(title)
+    
+    data_hist, bin_edges = np.histogram(data,bins=100)
+    data_hist2, bin_edges2 = np.histogram(data,bins=data_range) # i.e. binsize=1
+
+    # Output a frame image and histograms
+    ax1 = plt.subplot2grid((5,2), (0,0), rowspan=2, colspan=2)
+    ax2 = plt.subplot2grid((5,2), (2,0), rowspan=2)
+    ax3 = plt.subplot2grid((5,2), (2,1), rowspan=2)
+                    
+    plt.sca(ax1)
+    plt.imshow(data, vmin=0, vmax=max(50,np.percentile(data,99)))
+    plt.colorbar(label='ADU', shrink=0.9)
+    
+    plt.sca(ax2)
+    plt.stairs(data_hist,edges=bin_edges)
+    plt.xlabel('ADU')
+    plt.ylabel('Pixel Count')
+    plt.text(0.55,0.95,'Whole range',transform=ax2.transAxes)
+    plt.semilogy()
+    
+    plt.sca(ax3)
+    plt.stairs(data_hist2,edges=bin_edges2)
+    plt.xlabel('ADU')
+    plt.xlim(np.percentile(data,[1.e-2,100-1.e-2]))
+    plt.text(0.35,0.95,'0.01 - 99.99 percentile',transform=ax3.transAxes)
+    plt.semilogy()
+                    
+    plt.text(0.0, -0.3, summary_text, transform=ax2.transAxes, verticalalignment='top')
+
+    plt.subplots_adjust(hspace=0.25)
+    pdf.savefig()
+    plt.close()
 
 if __name__ == '__main__':
     '''
