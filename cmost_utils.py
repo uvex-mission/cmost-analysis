213bac3f57c651e1b642ee0378c8b8b3a11a8d82
Hannah <hannah.earnshaw@googlemail.com>
2025-05-23 13:01:04 -0700

    Updated get_temp() to read from new UVEX cryostat logging page

--- cmost_utils.py
+++ cmost_utils.py
@@ -33,7 +33,7 @@ def get_temp(cmost=1):
     
     # Determine which URL to look at for data
     if cmost == 1:
-        temp_url = 'https://sites.astro.caltech.edu/~detlab/cmost/'+year+'/'+datestring+'/temps.csv'
+        temp_url = 'https://sites.astro.caltech.edu/~detlab/uvex/'+year+'/'+datestring+'/temps.csv'
     elif cmost == 2:
         temp_url = 'https://sites.astro.caltech.edu/~detlab/cmost-jpl/'+year+'/'+datestring+'/temps.csv'
     else:
@@ -53,7 +53,7 @@ def get_temp(cmost=1):
     last_line = line.decode('utf-8').split(',')
     
     time = datetime.strptime(last_line[0],'%m/%d/%y %H:%M:%S')
-    temp = last_line[1] # This will not work for dates before ~01/07/2021
-    # If implementing past dates, make sure to look at column [2] instead
+    temp = last_line[2]
+    # If implementing past dates, make sure to look at column [1] for old CMOST temperature readings between 2021 and 2024
 
     return time.isoformat(), temp
